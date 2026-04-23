41c6f1140bdaf0276ac6272828d43f8677bceeb6
rajeevranjaniOS <31539778+rajeevranjaniOS@users.noreply.github.com>
2026-04-23 18:02:39 +0530

    Change initial values of a and b in Calculator
    Updated values of variables 'a' and 'b' in main method.

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,7 @@
 public class Calculator {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 5;
+        int a = 20;
+        int b = 20;
 
         int sum = a + b;
         int diff = a - b;
