028ad9163dbc65784652ed9abba83c5181868c1e
rajeevranjaniOS <31539778+rajeevranjaniOS@users.noreply.github.com>
2026-04-23 17:25:28 +0530

    Add basic Calculator class with arithmetic operations

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        int sum = a + b;
+        int diff = a - b;
+        int product = a * b;
+        int division = a / b;
+
+        System.out.println("Numbers: " + a + " and " + b);
+        System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + diff);
+        System.out.println("Multiplication: " + product);
+        System.out.println("Division: " + division);
+    }
+}
